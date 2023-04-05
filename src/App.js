18baad4ff31741746487ee7ff324be1a99336c39
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-04-05 11:39:18 +0200

    removed login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import "./App.css";
-import Login from "./pages/login.js";
-import Home from "./pages/home.js";
+import Home from "./pages/Home.js";
 import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-  const [accessToken, setAccessToken] = useState(null);
   return (
     <div>
       <Routes>
-        <Route path="/h" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
-        <Route
-          path="/"
-          element={
-            <Login accessToken={accessToken} setAccessToken={setAccessToken} />
-          }
-        />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,9 @@
+import Button from "../components/Button/index";
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <Button text="Search" tier="" minion-type="" golden="" keyword="" />
     </div>
   );
 };
