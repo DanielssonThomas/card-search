f34ee5a3fa00e38a9f97c9926cb4d6a564ab696f
DouglasLindahl <douglas.lindahl@gmail.com>
2023-04-05 10:11:00 +0200

    error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Login from './pages/Login.js';
+import './App.css';
+import Home from './pages/Home';
+import {Routes, Route, Link} from 'react-router-dom';
+
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path = "/" element={<Home />} />
+        <Route path = "/Home" element={<Home />} />
+        <Route path = "/login" element={<Login />} />
+      </Routes>
+     </>
+
+  );
 }
 
 export default App;
---
+++ src/pages/login.js
@@ -0,0 +1,11 @@
+import Button from '../components/Button/index'
+
+
+function Login(){
+    return(
+        <div class = "loginScreen">
+            <Button text="Login" />
+        </div>
+    );
+}
+export default Login;
