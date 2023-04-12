7315f7c7b820627b4d82badfd4e615e3fe7df4c7
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-04-12 10:37:35 +0200

    add style

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import Button from "../components/Button/index";
 import Card from "../components/Card";
 import { useState, useEffect } from "react";
-
+import "./style.css";
 
 const Home = () => {
   const [data, setData] = useState(null);
@@ -9,49 +9,42 @@ const Home = () => {
 
   const url =
     "https://eu.api.blizzard.com/hearthstone/cards?locale=en_US&gameMode=battlegrounds&tier=3&access_token=";
-  
-    useEffect(() => {
-      const clientSecret = process.env.REACT_APP_SECRET;
-      const clientId = process.env.REACT_APP_ID;
-  
-      const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
-      };
-  
-  
-      const fetchData = async () =>{
-        let response = await fetch(
-          "https://us.battle.net/oauth/token",
-          requestOptions
-        );
-        let data = await response.json();
-        let accessToken = data.access_token;
-        setAccessToken(accessToken);
-        response = await fetch(`${url}${accessToken}`);
-        data = await response.json();
-        setData(data);
-      }
-      fetchData();
-    }, []);
 
-    let cardsComponent = [];
-    if(data != null)
-    {
-      cardsComponent = data.cards.map((card) => (
-        <Card
-          key={card.id}
-          src = {card.image}
-          name = {card.name}
-        />
-      ));
-    }
+  useEffect(() => {
+    const clientSecret = process.env.REACT_APP_SECRET;
+    const clientId = process.env.REACT_APP_ID;
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    };
+
+    const fetchData = async () => {
+      let response = await fetch(
+        "https://us.battle.net/oauth/token",
+        requestOptions
+      );
+      let data = await response.json();
+      let accessToken = data.access_token;
+      setAccessToken(accessToken);
+      response = await fetch(`${url}${accessToken}`);
+      data = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
+  let cardsComponent = [];
+  if (data != null) {
+    cardsComponent = data.cards.map((card) => (
+      <Card key={card.id} src={card.image} name={card.name} />
+    ));
+  }
 
   return (
-    <div>
-      <h2>Home</h2>
-      {cardsComponent}
+    <div className="home">
+      <div className="cards-container">{cardsComponent}</div>
       {/* <Button text="Search" tier="" minion-type="" golden="" keyword="" /> */}
     </div>
   );
